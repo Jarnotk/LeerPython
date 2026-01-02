09ce9790be9ce2618ac8197398ce3f406b768b77
Jarnotk <jarnotk@gmail.com>
2026-01-02 01:26:38 +0100

    Les 01 bestand aangemaakt en geschreven.

---
+++ Les_01.py
@@ -0,0 +1,16 @@
+# Python is een programmeertaal. Dat houd in: 
+# - Je schrijft tekst 
+# - Python leest de geschreven tekst 
+# - De computer voert het uit
+# 
+# Python is op dit moment een snel groeiende programmeertaal. Dit komt vooral omdat Python makkelijk
+# te schrijven/lezen is, veel leerbronnen heeft en een grote community heeft.
+# 
+# In deze les leren we de basis van Python.
+# 
+# We beginnen met het schrijven van een eenvoudige tekst.
+# 
+# Onderstaande code zorgt ervoor dat de tekst "Hallo, ik leer Python" wordt weergegeven op je scherm.
+# Dat komt door de "print()" functie.
+
+print("Hallo, ik leer Python")
